148af5036af64b2e61ef8d32ace9b362b15adfc1
Niko Fink <nfink95@gmail.com>
2017-07-20 09:07:56 +0200

    set timeout on notification when done

--- blue_audio_con/notification.py
+++ blue_audio_con/notification.py
@@ -15,26 +15,29 @@ STATUS_FAILED = 2
 class Notification(ContextDecorator):
     def __init__(self):
         self.notification = None
-        self.active = False
         self.logger = logging.getLogger()
 
     def __enter__(self):
         Notify.init('blue-audio-con')
-        self.active = True
         return self
 
     def __exit__(self, exc_type, exc_value, exc_traceback):
-        self.active = False
         if exc_type:
             self.update("Failed: %s" % repr(exc_type), STATUS_FAILED)
+        if self.notification:
+            self.notification.set_hint("resident", GLib.Variant('b', False))
+            self.notification.set_timeout(-1)  # NOTIFY_EXPIRES_DEFAULT
+            self.notification.show()
         Notify.uninit()
+
         return False
 
     def update(self, text=None, status=STATUS_IN_PROGRESS):
-        if status == STATUS_FAILED:
-            self.logger.error(text)
-        else:
-            self.logger.info(text)
+        if text:
+            if status == STATUS_FAILED:
+                self.logger.error(text)
+            else:
+                self.logger.info(text)
 
         icon = {
             STATUS_IN_PROGRESS: "audio-volume-low",
@@ -43,9 +46,10 @@ class Notification(ContextDecorator):
         }[status]
         if not self.notification:
             self.notification = Notify.Notification.new("Bluetooth Audio Device Assistant", text, icon)
-        else:
+        elif text:
             self.notification.update("Bluetooth Audio Device Assistant", text, icon)
 
-        self.notification.set_urgency(0 if status == STATUS_IN_PROGRESS else 1)  # TODO popup if == 1
-        self.notification.set_hint("resident", GLib.Variant('b', True if self.active else False))
+        self.notification.set_urgency(0 if status == STATUS_IN_PROGRESS else 1)
+        self.notification.set_hint("resident", GLib.Variant('b', True))
+        self.notification.set_timeout(0)  # NOTIFY_EXPIRES_NEVER
         self.notification.show()
